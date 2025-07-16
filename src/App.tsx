112a050c0a1f4754a405778879e3df27ed592ac7
Satyendra <satya@satyendra.in>
2025-07-16 14:47:37 +0530

    fix: Simplify preloader hiding logic by removing unnecessary timeout

--- src/App.tsx
+++ src/App.tsx
@@ -49,9 +49,7 @@ const App = () => {
   useEffect(() => {
     const preloader = document.getElementById('preloader');
     if (preloader) {
-      setTimeout(() => {
-        preloader.classList.add('hidden');
-      }, 200);
+      preloader.classList.add('hidden')
     }
   }, []);
 
